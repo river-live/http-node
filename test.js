7243f3479dec1922771a63ab3862a90d8400ced4
William Jackson <williampj@gmail.com>
2020-08-13 16:05:23 +0300

    latest changes

--- test.js
+++ test.js
@@ -1,7 +1,13 @@
 const River = require('./lib/river');
 
-const host = 'https://djp47y1957.execute-api.us-east-2.amazonaws.com';
+const host =
+  'https://78kqtukaic.execute-api.us-east-2.amazonaws.com/apps';
+// const host = 'djp47y1957.execute-api.us-east-2.amazonaws.com';
 
 const river = new River({ host });
 
 river.publish('one', 'event', { 'Data String': 'data string' });
+
+exports.publishMessage = function () {
+  river.publish('one', 'event', { 'Data String': 'data string' });
+};
