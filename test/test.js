398e20494a9c97074a66deebe44d4fb23fbbd736
Mark Hyams <markhyams@gmail.com>
2020-08-14 16:45:40 -0600

    fixed test path

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-const River = require("./lib/river");
+const River = require("../lib/river");
 const host = "https://xrrjfc1x9j.execute-api.us-east-1.amazonaws.com/publish"; // hardcoded for testing purposes with one specific deploy
 const key = "xrrjfc1x9j"; // hardcoded for testing purposes with one specific deploy
 const river = new River({ host, key });
