2a9914445228c2aaece379bb501fe688e127b25b
Catherine Emond <catherine.dnome@gmail.com>
2020-08-14 14:41:25 -0400

    small fixes

--- test.js
+++ test.js
@@ -1,8 +1,7 @@
-const River = require('./lib/river');
+const River = require("./lib/river");
 
-const host =
-  'https://y7bt1s686j.execute-api.us-east-1.amazonaws.com/apps';
+const host = "https://y7bt1s686j.execute-api.us-east-1.amazonaws.com/apps";
 
 const river = new River({ host });
 
-river.publish('one', 'event', { 'Data String': 'data string' });
+river.publish("one", "event", { "Data String": "data string" });
